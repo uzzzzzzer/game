4756ac2db900cd3dd9f4512aaff027c282411862
uzzzzzzer <79088180+uzzzzzzer@users.noreply.github.com>
2021-05-14 17:11:20 +0300

    Update BotLoader.js

--- src/ai/BotLoader.js
+++ src/ai/BotLoader.js
@@ -43,5 +43,5 @@ BotLoader.prototype.getName = function() {
 		name = "bot" + ++this.nameIndex;
 	}
 	
-	return this.randomNames[index]";
+	return this.randomNames[Math.floor(Math.random() * this.randomNames.length)];
 }
